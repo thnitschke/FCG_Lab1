8fc92eb6c76e417778ffa2a781599752f13b71e1
thnitschke <thiago.nitschke@gmail.com>
2018-11-10 17:10:47 -0200

    Commit inicial

---
+++ cmake-build-debug/CMakeFiles/Laboratorio_1_Codigo_Fonte.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laboratorio_1_Codigo_Fonte.dir/src/glad.c.o"
+  "CMakeFiles/Laboratorio_1_Codigo_Fonte.dir/src/main.cpp.o"
+  "Laboratorio_1_Codigo_Fonte.pdb"
+  "Laboratorio_1_Codigo_Fonte"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Laboratorio_1_Codigo_Fonte.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
